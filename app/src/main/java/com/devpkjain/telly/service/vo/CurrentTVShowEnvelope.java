f53e8be2d5ae3780be64c9bc967df1564f70a281
Pankaj Kumar Jain <devpkjain@gmail.com>
2015-06-21 13:26:39 -0400

    Performance Improvements using Parcelable

--- app/src/main/java/com/devpkjain/telly/service/vo/CurrentTVShowEnvelope.java
+++ app/src/main/java/com/devpkjain/telly/service/vo/CurrentTVShowEnvelope.java
@@ -1,11 +1,24 @@
 package com.devpkjain.telly.service.vo;
 
-import com.google.gson.annotations.SerializedName;
+import android.os.Parcel;
+import android.os.Parcelable;
 
-public class CurrentTVShowEnvelope {
-  @SerializedName("num") public int num;
-  @SerializedName("img") public String img;
-  @SerializedName("lnk") public String lnk;
-  @SerializedName("name") public String name;
-  @SerializedName("air") public String air;
+public class CurrentTVShowEnvelope implements Parcelable {
+  public int num;
+  public String img;
+  public String lnk;
+  public String name;
+  public String air;
+
+  @Override public int describeContents() {
+    return 0;
+  }
+
+  @Override public void writeToParcel(Parcel dest, int flags) {
+    dest.writeInt(num);
+    dest.writeString(img);
+    dest.writeString(lnk);
+    dest.writeString(name);
+    dest.writeString(air);
+  }
 }
